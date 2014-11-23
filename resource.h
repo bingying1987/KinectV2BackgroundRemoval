5ca1c726298a0ae05a18871aba4178c8b088befd
hughwz <hughwz@126.com>
2014-11-23 12:51:29 +0800

    KinectV2BackgroundRemoval
    KinectV2BackgroundRemoval + screenshot
    save screenshot to C:\Users\Pictures

---
+++ resource.h
@@ -0,0 +1,70 @@
+//------------------------------------------------------------------------------
+// <copyright file="Resource.h" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by CoordinateMappingBasics.rc
+//
+#define IDI_APP                         100
+#define IDD_APP                         100
+#define IDC_VIDEOVIEW                   1000
+#define IDC_STATUS                      1001
+#define IDC_BUTTON_SCREENSHOT           1002
+#define IDC_BUTTON_BACKGROUND			1003
+#define IDC_BUTTON_AUTO					1004
+
+#define IDC_BUTTON_BG1					2000
+#define IDC_BUTTON_BG2					2001
+#define IDC_BUTTON_BG3					2002
+#define IDC_BUTTON_BG4					2003
+#define IDC_BUTTON_BG5					2004
+#define IDC_BUTTON_BG6					2005
+#define IDC_BUTTON_BG7					2006
+#define IDC_BUTTON_BG8					2007
+#define IDC_BUTTON_BG9					2008
+#define IDC_BUTTON_BG10					2009
+#define IDC_BUTTON_BG11					2010
+#define IDC_BUTTON_BG12					2011
+#define IDC_BUTTON_BG13					2012
+#define IDC_BUTTON_BG14					2013
+#define IDC_BUTTON_BG15					2014
+#define IDC_BUTTON_BG16					2015
+#define IDC_BUTTON_BG17					2016
+#define IDC_BUTTON_BG18					2017
+#define IDC_BUTTON_BG19					2018
+#define IDC_BUTTON_BG20					2019
+#define IDC_BUTTON_BG21					2020
+#define IDC_BUTTON_BG22					2021
+#define IDC_BUTTON_BG23					2022
+#define IDC_BUTTON_BG24					2023
+#define IDC_BUTTON_BG25					2024
+#define IDC_BUTTON_BG26					2025
+#define IDC_BUTTON_BG27					2026
+#define IDC_BUTTON_BG28					2027
+#define IDC_BUTTON_BG29					2028
+#define IDC_BUTTON_BG30					2029
+#define IDC_BUTTON_BG31					2030
+#define IDC_BUTTON_BG32					2031
+#define IDC_BUTTON_BG33					2032
+#define IDC_BUTTON_BG34					2033
+#define IDC_BUTTON_BG35					2034
+#define IDC_BUTTON_BG36					2035
+#define IDC_BUTTON_BG37					2036
+#define IDC_BUTTON_BG38					2037
+#define IDC_BUTTON_BG39					2038
+#define IDC_BUTTON_BG40					2039
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_SYMED_VALUE           102
+#endif
+#endif
